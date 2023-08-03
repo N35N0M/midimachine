f3aa0bf0b7757b5269e1b2bda27e683ec37c5d61
Kris Monsen <kristoffer.venaes.monsen@appear.net>
2023-08-03 21:38:27 +0200

    Add small livetest routine but holifackingshit it works

--- main.py
+++ main.py
@@ -106,8 +106,82 @@ def main():
 
 
 
-
+# Channel 1-96 (96ch, 3ch per pixel): Lightbar left
+# Channel 97-192 (96ch, 3ch per pixel): Lightbar Center
+# Channel 193-288 (96ch, 3ch per pixel): Lightbar Right
+# Channel 289-295 (7ch, ch 1 = master dimmer?, ch2,3,4 = RGB?): Dragon left eye left
+# Channel 296-302(7ch): Dragon left eye right
+# Channel 303-308 (6ch, ch1 smoke emit, ch2 hardcode 0 for color select, ch3-5 is RGB): Dragon left smoke
+# Channel 309-315(7ch): Dragon right eye left
+# Channel 316-322 (7ch): Dragon right eye right
+# Channel 323-328 (6ch, ch1 smoke emit, ch2 hardcode 0 for color select, ch3-5 is RGB): Dragon right smoke
 if __name__ == "__main__":
+    print("Clearing channels!")
+    for i in range(1, 513):
+        ctrl.set_channel(i, 0)
+    ctrl.submit()
+
+    print("Testing left sidebar!")
+    ctrl.set_channel(1, 255)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing center sidebar!")
+    ctrl.set_channel(97, 255)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing right sidebar!")
+    ctrl.set_channel(193, 255)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing left-dragon left-eye")
+    ctrl.set_channel(289, 255)
+    ctrl.set_channel(291, 255)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing left-dragon right-eye")
+    ctrl.set_channel(296, 255)
+    ctrl.set_channel(298, 255)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing left-dragon smoke")
+    ctrl.set_channel(303, 255)
+    ctrl.set_channel(304, 0)
+    ctrl.set_channel(305, 255)
+    ctrl.submit()
+    time.sleep(1)
+    ctrl.set_channel(303, 0)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing right-dragon left-eye")
+    ctrl.set_channel(309, 255)
+    ctrl.set_channel(311, 255)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing right-dragon right-eye")
+    ctrl.set_channel(316, 255)
+    ctrl.set_channel(318, 255)
+    ctrl.submit()
+    time.sleep(1)
+
+    print("Testing right-dragon smoke")
+    ctrl.set_channel(323, 255)
+    ctrl.set_channel(324, 0)
+    ctrl.set_channel(325, 255)
+    ctrl.submit()
+    time.sleep(1)
+    ctrl.set_channel(323, 0)
+    ctrl.submit()
+    time.sleep(1)
+
+
+
     threading.Thread(target=lambda: app.run(debug=False, use_reloader=False)).start()
     main()
 
